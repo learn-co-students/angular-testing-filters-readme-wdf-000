b8a949a87dea09e8ac26e750a22b66351955a74e
GordonNY <gordonsdev@gmail.com>
2016-08-16 08:48:34 -0400

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,4 +1,18 @@
 describe('ContactController', function () {
 
-
+	var $controller;
+ 
+    beforeEach(module('app'));
+ 
+    beforeEach(inject(function (_$controller_) {
+        $controller = _$controller_;
+    }));
+ 
+ 
+    it('should filter the results correctly', function () {
+        var $scope = {};
+        $controller('ContactController as vm', {$scope: $scope});
+ 
+        expect($scope.vm.filteredList[0]).toEqual({name: 'Bob'});
+    });
 });
